88e8e8224e32b0ca58a04e8fbc09e0a531068a49
royccg <1804130207@njtech.edu.cn>
2020-03-19 16:16:41 +0800

    代码编程 0319

---
+++ pyqt5_tech/代码创建窗口/FirstMainWin.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow,QApplication
+
+# 添加图标
+from PyQt5.QtGui import QIcon
+
+# 面向对象编程，编写类
+class FirstMainWin(QMainWindow):
+    #  初始化，
+    def __init__(self,parent =None):
+        # 定义父类
+        super(FirstMainWin,self).__init__(parent)
+
+        # 设置主窗口的标题
+        self.setWindowTitle("第一个主窗口应用")
+
+        # 设置主窗口的尺寸
+        self.resize(500,300)
+        # 状态栏
+        self.status = self.statusBar()
+
+        self.status.showMessage("只存在5秒的消息",5000)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    # 设置程序的图标
+    app.setWindowIcon(QIcon("lufei.ico"))
+    main = FirstMainWin()
+    main.show()
+
+    sys.exit(app.exec_())
